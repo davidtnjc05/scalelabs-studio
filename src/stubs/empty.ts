56cd255b8b6f1c4e427ad2cd6deb70139d895ed3
davidtnjc05 <david.tunjic@scalelabsstudio.com>
2026-06-07 22:15:05 +0200

    Create empty.ts

---
+++ src/stubs/empty.ts
@@ -0,0 +1,8 @@
+// Leere Stub-Datei fuer Lovable/TanStack-Start Pakete
+// Diese werden im SPA-Mode nicht gebraucht
+export default {};
+export const createServerFn = () => () => {};
+export const createMiddleware = () => () => {};
+export const json = (data) => data;
+export const redirect = (url) => ({ url });
+export const useServerFn = (fn) => fn;
