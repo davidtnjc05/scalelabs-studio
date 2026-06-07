9585b147e00243c6a3bc8a783a60aec07dfb0e22
davidtnjc05 <david.tunjic@scalelabsstudio.com>
2026-06-07 23:28:16 +0200

    Fix createServerFn stub to return chainable object

--- src/stubs/empty.ts
+++ src/stubs/empty.ts
@@ -1,8 +1,8 @@
 // Leere Stub-Datei fuer Lovable/TanStack-Start Pakete
 // Diese werden im SPA-Mode nicht gebraucht
 export default {};
-export const createServerFn = () => () => {};
-export const createMiddleware = () => () => {};
-export const json = (data) => data;
-export const redirect = (url) => ({ url });
-export const useServerFn = (fn) => fn;
+export const createServerFn = (_?: any) => { const c: any = { inputValidator: () => c, handler: () => c, middleware: () => c, validator: () => c }; return c; };
+export const createMiddleware = () => () => () => {};
+export const json = (data: any) => data;
+export const redirect = (url: string) => ({ url });
+export const useServerFn = (fn: any) => fn;
